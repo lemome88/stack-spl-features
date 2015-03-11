2f52a3429933e068c0072f20fc89c717a8ed2163
letimome <leticia.montalvillo@ehu.es>
2015-03-11 17:07:02 +0100

    Update Stack.java

--- overFlow/util/Stack.java
+++ overFlow/util/Stack.java
@@ -4,8 +4,11 @@ public class Stack {
 	private int countMax = 50;
 	
 	public void push(Object obj) {
-		if(countMax==50)
+		if(countMax==50){
+			System.out.println("max reached);
 			return null;
+			
+		}
 		else original(obj);
 	}
 	
